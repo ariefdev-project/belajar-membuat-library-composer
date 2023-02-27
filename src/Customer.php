e5ee535fecdedd567f54a6da5cff1c039dba2b8b
= <=>
2023-02-27 08:40:19 +0800

    Menambahkan Default Value

--- src/Customer.php
+++ src/Customer.php
@@ -9,7 +9,7 @@ class Customer
   {
   }
 
-  public function sayHello(string $name)
+  public function sayHello(string $name = "GUEST")
   {
     echo "Hello $name,my name is $this->name" . PHP_EOL;
   }
