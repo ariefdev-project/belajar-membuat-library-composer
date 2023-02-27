6a4fa968140b52965b8ecf55c3fb2df0e1bb8d81
= <=>
2023-02-27 08:18:56 +0800

    Belajar Membuat Library Dengan Composer

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AriefDev\LibraryComposer;
+
+class Customer
+{
+
+  public function __construct(private string $name)
+  {
+  }
+
+  public function sayHello(string $name)
+  {
+    echo "Hello $name,my name is $this->name" . PHP_EOL;
+  }
+}
